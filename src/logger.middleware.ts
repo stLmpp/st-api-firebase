3c835e4a85c7dfb00f260e47b07c38620205ce07
stLmpp <gui.stlmpp@gmail.com>
2024-05-03 16:20:38 -0300

    fix: hide authorization and x-api-key headers from logger

--- src/logger.middleware.ts
+++ src/logger.middleware.ts
@@ -1,3 +1,5 @@
+import { IncomingHttpHeaders } from 'node:http';
+
 import { NestMiddleware } from '@nestjs/common';
 import type { NextFunction, Request, Response } from 'express';
 
@@ -6,8 +8,10 @@ import { Logger } from './logger.js';
 export class LoggerMiddleware implements NestMiddleware {
   private readonly logger = Logger.create(this);
 
+  private readonly headersToHide = ['x-api-key', 'authorization'];
+
   use(req: Request, _: Response, next: NextFunction): void {
-    const { body, method: _method, params, query, originalUrl, headers } = req;
+    const { body, method: _method, params, query, originalUrl } = req;
     const method = _method.toUpperCase();
     this.logger.debug(`Received request at ${method} ${originalUrl}`, {
       url: originalUrl,
@@ -15,8 +19,18 @@ export class LoggerMiddleware implements NestMiddleware {
       body: JSON.stringify(body),
       params: JSON.stringify(params),
       query: JSON.stringify(query),
-      headers: JSON.stringify(headers),
+      headers: JSON.stringify(this.cleanHeaders(req)),
     });
     next();
+  }
+
+  private cleanHeaders(req: Request): IncomingHttpHeaders {
+    const headers = { ...req.headers };
+    let index = this.headersToHide.length;
+    while (index--) {
+      const key = this.headersToHide[index]!;
+      headers[key] = '[REDACTED]';
+    }
+    return headers;
   }
 }
