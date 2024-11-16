d57b36b2c51eb70b424d998a62a2113e43b93aba
stLmpp <gui.stlmpp@gmail.com>
2024-11-16 15:20:28 -0300

    fix: log only path on logger middleware

--- src/logger.middleware.ts
+++ src/logger.middleware.ts
@@ -18,9 +18,10 @@ export function loggerMiddleware(logger: Logger): MiddlewareHandler {
   return async (c, next) => {
     const method = c.req.method;
     const url = c.req.url;
+    const path = c.req.path;
     const params = c.req.param();
     const query = c.req.query();
-    logger.debug(`[${method} ${url}] Request`, {
+    logger.debug(`[${method} ${path}] Request`, {
       url,
       method,
       params: JSON.stringify(params),
@@ -28,7 +29,7 @@ export function loggerMiddleware(logger: Logger): MiddlewareHandler {
       headers: JSON.stringify(cleanHeaders(c.req)),
     });
     await next();
-    logger.debug(`[${method} ${url}] Response`, {
+    logger.debug(`[${method} ${path}] Response`, {
       url,
       method,
       status: c.res.status,
