7dc9817478c1599645265d82cb58745fd7157d3f
stLmpp <gui.stlmpp@gmail.com>
2025-06-22 22:41:49 -0300

    fix: enhance inject function with optional parameter handling
    - Support optional parameter in `inject` function for improved flexibility.
    - Bumped package version from 6.5.6 to 6.5.7.

--- src/common/inject.ts
+++ src/common/inject.ts
@@ -5,10 +5,25 @@ import { InjectionToken } from '@stlmpp/di';
 
 export const APP_SYMBOL = Symbol('APP_SYMBOL');
 
-export function inject<T>(type: Class<T> | InjectionToken<T>): T {
+export function inject<T>(
+  type: Class<T> | InjectionToken<T>,
+  options: { optional: true },
+): T | undefined;
+export function inject<T>(
+  type: Class<T> | InjectionToken<T>,
+  options: { optional: false },
+): T;
+export function inject<T>(
+  type: Class<T> | InjectionToken<T>,
+  options?: { optional: boolean },
+): T;
+export function inject<T>(
+  type: Class<T> | InjectionToken<T>,
+  options?: { optional: boolean },
+): T | undefined {
   const app = getStateMetadataKey(APP_SYMBOL) as HonoApp<Hono> | undefined;
   if (!app) {
     throw new Error('App is not running on context');
   }
-  return app.injector.get(type);
+  return app.injector.get(type, options);
 }
