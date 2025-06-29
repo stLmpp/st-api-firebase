5f062c03863346da68f6a3e21e8f87334c3246a0
stLmpp <gui.stlmpp@gmail.com>
2025-06-29 11:39:32 -0300

    feat(firebase): add provider factory methods for collection and bucket
    - Added `createCollectionProvider` in `FirebaseAdminFirestore` for dynamic collection injection.
    - Added `createBucketProvider` in `FirebaseAdminStorage` for dynamic bucket injection.
    - Exported the new methods in the main index file for accessibility.
    - Bumped package version from 6.5.8 to 6.6.0.

--- src/firebase-admin/firebase-admin-firestore.ts
+++ src/firebase-admin/firebase-admin-firestore.ts
@@ -1,7 +1,23 @@
-import { Firestore } from 'firebase-admin/firestore';
+import { Firestore, CollectionReference } from 'firebase-admin/firestore';
 
 import { getClazz } from '../common/get-clazz.js';
-import { Injectable } from '@stlmpp/di';
+import { FactoryProvider, Injectable, InjectionToken } from '@stlmpp/di';
 
 @Injectable()
 export class FirebaseAdminFirestore extends getClazz<Firestore>() {}
+
+export function createCollectionProvider(
+  collection: string,
+): [InjectionToken<CollectionReference>, FactoryProvider] {
+  const token = new InjectionToken<CollectionReference>(
+    `st-api-firebase-collection-${collection}`,
+  );
+  return [
+    token,
+    new FactoryProvider(
+      token,
+      (firestore) => firestore.collection(collection),
+      [FirebaseAdminFirestore],
+    ),
+  ];
+}
--- src/firebase-admin/firebase-admin-storage.ts
+++ src/firebase-admin/firebase-admin-storage.ts
@@ -1,7 +1,24 @@
 import { Storage } from 'firebase-admin/storage';
 
 import { getClazz } from '../common/get-clazz.js';
-import { Injectable } from '@stlmpp/di';
+import { FactoryProvider, Injectable, InjectionToken } from '@stlmpp/di';
+import { Bucket } from '@google-cloud/storage';
 
 @Injectable()
 export class FirebaseAdminStorage extends getClazz<Storage>() {}
+
+export function createBucketProvider(
+  bucketName: string,
+): [InjectionToken<Bucket>, FactoryProvider] {
+  const token = new InjectionToken<Bucket>(
+    `st-api-firebase-bucket-${bucketName}`,
+  );
+  return [
+    token,
+    new FactoryProvider(
+      token,
+      (storage) => storage.bucket(bucketName) as never,
+      [FirebaseAdminStorage],
+    ),
+  ];
+}
